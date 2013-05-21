3e229c417375612b0465579a4fae525991aa13da
Owen Mundy <omundy@gmail.com>
2013-05-21 16:58:24 -0400

    consolidated licenses

--- inc/functions/data_get_fb.php
+++ inc/functions/data_get_fb.php
@@ -1,29 +1,5 @@
 <?php
 
-/**
- * 	Copyright 2011 Owen Mundy 
- *
- *	This file is part of Give Me My Data.
- *
- *	Give Me My Data is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
- *	
- *	Give Me My Data is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *	
- *	You should have received a copy of the GNU General Public License
- *	along with Give Me My Data.  If not, see <http://www.gnu.org/licenses/>.
- */ 
-
-
-
-
-
- 
 
 /**
  * Keeps track of all data types. Used by web form, Javascript, and core functions referenced by FQL.
