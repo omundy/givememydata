de501da2a923c5ff2d1ac4b6bafab66fccddd88e
Owen Mundy <omundy@gmail.com>
2024-01-04 14:18:51 -0500

    Updates

---
+++ _app/immutable/chunks/paths.PR6tz993.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1ook456?.base??"/givememydata",a=globalThis.__sveltekit_1ook456?.assets??s;export{a,s as b};
--- _app/immutable/chunks/paths.hjg18HGV.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_kax9pa?.base??"/givememydata",a=globalThis.__sveltekit_kax9pa?.assets??s;export{a,s as b};
