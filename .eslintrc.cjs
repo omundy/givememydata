4e6ea28463253ecb23604a563a42f46e40864296
Owen Mundy <omundy@gmail.com>
2026-08-02 18:51:16 -0400

    Upgrade Astro, related improvements

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,5 +10,19 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
+			}
+		}
+	],
+	globals: {
+		dataLayer: 'writable',
+		gtag: 'writable'
 	}
 };
